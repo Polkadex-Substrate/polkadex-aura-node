83fb205ee29235bb9bbf1723c66151bc326415b5
Ubuntu <ubuntu@ip-172-31-20-232.ap-southeast-1.compute.internal>
2021-01-05 07:16:17 +0000

    Update Bob

--- scripts/validator_bob.sh
+++ scripts/validator_bob.sh
@@ -1,2 +1,2 @@
 rm -r -f /tmp/bob/
-../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --bob --port 30333 --ws-port 9944 --rpc-port 9934 --validator --node-key 0000000000000000000000000000000000000000000000000000000000000002
+../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --bob --port 30333 --ws-port 9955 --ws-external --rpc-port 9934 --rpc-methods=unsafe --validator --node-key 0000000000000000000000000000000000000000000000000000000000000002 --execution Native
